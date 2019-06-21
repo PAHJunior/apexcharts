ba7f0e3c693e2d10ea6365413ac566ab2359d0ea
PAHJunior <pahjunior@outlook.com>
2019-06-21 20:57:52 -0300

    first commit :alien:

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+
+        <q-toolbar-title>
+          Exemplo - Graficos
+        </q-toolbar-title>
+
+        <div> v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <q-card dark bordered class="text-primary">
+        <q-card-section>
+          <div class="text-h6">Grafico de linha</div>
+          <q-btn @click="updateChart" icon="check"/>
+        </q-card-section>
+
+        <q-separator dark inset />
+
+        <q-card-section>
+          <apexchart width="580" type="line" height=350 :options="options" :series="series" />
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+
+</template>
+
+<style>
+#chart{
+  height: 50%;
+  width: 50%;
+}
+</style>
+
+<script>
+import ApexCharts from 'apexcharts'
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+  name: 'PageIndex',
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      options: {
+        chart: {
+          
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        },
+      },
+      series: [{
+        name: 'Total',
+        data: [30, 40, 45, 50, 49, 60, 70, 81]
+      }],
+      theme: {
+        monochrome: {
+          enabled: true,
+          color: '#255aee',
+          shadeTo: 'light',
+          shadeIntensity: 0.65
+        }
+      }
+    }
+  },
+  methods: {
+    updateChart() {
+      const max = 90;
+      const min = 20;
+      const newData = this.series[0].data.map(() => {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+      })
+      // In the same way, update the series option
+      this.series = [{
+        data: newData
+      }]
+    }
+  }
+}
+</script>
